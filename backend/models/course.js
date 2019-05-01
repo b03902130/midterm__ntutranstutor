b7728d10f991072bdca8c803fe697eba2e35c9c7
b03902130 <b03902130@ntu.edu.tw>
2019-05-02 01:58:10 +0800

    define all database schema

--- backend/models/course.js
+++ backend/models/course.js
@@ -3,14 +3,22 @@ const Schema = mongoose.Schema
 
 // Creating a schema, sort of like working with an ORM
 const CourseSchema = new Schema({
-    googleid: {
+    teacherid: {
         type: String,
-        required: [true, 'Googleid field is required.']
+        required: [true, 'teacherid field is required.']
+    },
+    subjectid: {
+        type: String,
+        required: [true, 'subjectid field is required.']
+    },
+    price: {
+        type: String,
+        required: [true, 'price field is required.']
     },
     description: {
         type: String,
-        required: [true, 'Description field is required.']
-    },
+        required: [true, 'description field is required.']
+    }
 })
 
 // Creating a table within database with the defined schema
--- backend/models/subject.js
+++ backend/models/subject.js
@@ -3,14 +3,10 @@ const Schema = mongoose.Schema
 
 // Creating a schema, sort of like working with an ORM
 const SubjectSchema = new Schema({
-    googleid: {
+    name: {
         type: String,
-        required: [true, 'Googleid field is required.']
-    },
-    description: {
-        type: String,
-        required: [true, 'Description field is required.']
-    },
+        required: [true, 'name field is required.']
+    }
 })
 
 // Creating a table within database with the defined schema
